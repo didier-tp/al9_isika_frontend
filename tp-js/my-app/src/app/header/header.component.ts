c8a0bb4ab49c1b2c4fc283c3531c65c22c2579f5
didierDefrance <didier@d-defrance.fr>
2021-09-15 16:41:37 +0200

    avec menu deroulant

--- tp-js/my-app/src/app/header/header.component.ts
+++ tp-js/my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
@@ -10,10 +11,27 @@ export class HeaderComponent implements OnInit {
 
   @Input()
   public titre : string = "titre_par_defaut";
- 
-  constructor(public preferencesService : PreferencesService) { }
 
+  myMenuDefs : MenuDefinition[] = [
+    { label : "Pour admin" , 
+      children : [
+        { label : "login" , path : "ngr-login" } ,
+        { label : "basic" , path : "ngr-basic" } ,
+        { divider : true },
+        { label : "welcome" , path : "ngr-welcome" }
+      ]
+    },
+    { label : "basic" , path : "ngr-basic" } , 
+    { label : "welcome" , path : "ngr-welcome" }
+    ];
+ 
+  constructor(public preferencesService : PreferencesService) { 
+      console.log("dans constructor de HeaderComponent , titre="+this.titre)
+  }
+
+  //ngOnInit() ressemble à @PostConstruct de java
   ngOnInit(): void {
+    console.log("dans ngOnInit de HeaderComponent , titre="+this.titre)
   }
 
 }
